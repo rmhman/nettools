38c7d247c36ae1a2a02d5f0032ac277b75745ab3
Ross Heilman <ross.heilman@citizensbank.com>
2024-01-02 17:55:09 -0500

    RossUpdate

--- shared/python/pyATS/setvars.sh
+++ shared/python/pyATS/setvars.sh
@@ -16,10 +16,20 @@ echo "PYATS_USER: $PYATS_USER"
 echo "PYATS_PASS: $PYATS_PASS"
 echo "PYATS_ENABLE_PASS: $PYATS_ENABLE_PASS"
 
-# Append the export commands to the shell configuration file for persistence
-echo "export PYATS_USER=\"$user\"" >> ~/.zshrc
-echo "export PYATS_PASS='$pass'" >> ~/.zshrc
-echo "export PYATS_ENABLE_PASS='$enable_pass'" >> ~/.zshrc
+# Check if export commands already exist in .zshrc file
+if grep -q "export PYATS_USER=" ~/.zshrc && grep -q "export PYATS_PASS=" ~/.zshrc && grep -q "export PYATS_ENABLE_PASS=" ~/.zshrc; then
+    # Update export commands in .zshrc file
+    sed -i "s/export PYATS_USER=.*/export PYATS_USER=\"$user\"/" ~/.zshrc
+    sed -i "s/export PYATS_PASS=.*/export PYATS_PASS='$pass'/" ~/.zshrc
+    sed -i "s/export PYATS_ENABLE_PASS=.*/export PYATS_ENABLE_PASS='$enable_pass'/" ~/.zshrc
+    echo "Export commands updated in .zshrc file."
+else
+    # Append export commands to .zshrc file
+    echo "export PYATS_USER=\"$user\"" >> ~/.zshrc
+    echo "export PYATS_PASS='$pass'" >> ~/.zshrc
+    echo "export PYATS_ENABLE_PASS='$enable_pass'" >> ~/.zshrc
+    echo "Export commands added to .zshrc file."
+fi
 
 # Source the shell configuration file to apply changes immediately
 source ~/.zshrc
